b79823bc34ba142164b5888fab53521b666194b0
schwarzie2478 <schwarzie2478@hotmail.com>
2016-11-30 12:19:29 +0100

    Started with CustomerDtoMapper

--- EFRelationTests/Mappers/CustomerMap.cs
+++ EFRelationTests/Mappers/CustomerMap.cs
@@ -2,6 +2,7 @@
 using AutoMapper;
 using EFRelationTests.Model;
 using EFRelationTests.Contracts;
+using EFRelationTests.Repositories;
 
 namespace EFRelationTests.Mappers
 {
@@ -13,4 +14,32 @@ namespace EFRelationTests.Mappers
             Mapper.CreateMap<CustomerDto, Customer>();
         }
     }
-}
+    public class CustomerDtoMapper
+    {
+
+        private readonly ICustomerRepository _customerRepository;
+        private IMapper _mapper;
+
+        public CustomerDtoMapper(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+            _mapper = AutoMapperConfiguration.GetMapper();
+        }
+
+        public Customer Map(CustomerDto customer)
+        {
+            
+            var cust = _customerRepository.Get(customer.Id);
+            if(cust != null)
+            {
+                var newCustomer
+                //Create
+            }else
+            {
+                //Update data
+
+                //UPdate Order if neccesary otherwire create
+            }
+        }
+
+    }
